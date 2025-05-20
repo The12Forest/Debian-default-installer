d7b74416289dee6b4d4b8e8b2cb71fe0e8ca0034
The12Forest <manuel@steka.ch>
2025-05-20 22:00:42 +0200

    First version

---
+++ install.sh
@@ -0,0 +1,47 @@
+su-
+apt install sudo curl -y
+usermod -aG sudo $USER
+newgrp sudo
+
+
+sudo apt-get update
+sudo apt-get install ca-certificates -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo docker network create -d macvlan --subnet 192.168.188.0/23 --gateway 192.168.188.1 -o parrent=enp1s0 VIP
+
+sudo usermod -aG docker $USER
+
+docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes -v /:/host portainer/agent:2.21.4
+
+
+# Cockpit
+
+su -
+
+curl -sSL https://repo.45drives.com/setup | sudo bash
+. /etc/os-release
+echo "deb http://deb.debian.org/debian ${VERSION_CODENAME}-backports main" > \
+    /etc/apt/sources.list.d/backports.list
+apt update
+apt install -t ${VERSION_CODENAME}-backports cockpit -y
+
+exit
+
+sudo apt install samba cockpit-storaged cockpit-networkmanager cockpit-file-sharing cockpit-identities  cockpit-benchmark -y
+
+sudo bash -c 'mkdir -p /etc/systemd/system/cockpit.socket.d && echo -e "[Socket]\nListenStream=\nListenStream=443" > /etc/systemd/system/cockpit.socket.d/override.conf && systemctl daemon-reload && systemctl restart cockpit.socket'
+
+sudo systemctl restart cockpit.socket
+
+
